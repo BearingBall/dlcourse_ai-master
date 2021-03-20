0b4f1fb387cfbc77815e8e3058c4d482ad72330f
BearingBall <74752877+BearingBall@users.noreply.github.com>
2021-03-20 16:54:31 +0700

    Initial commit

---
+++ assignments/assignment1/metrics.py
@@ -0,0 +1,67 @@
+def binary_classification_metrics(prediction, ground_truth):
+    '''
+    Computes metrics for binary classification
+
+    Arguments:
+    prediction, np array of bool (num_samples) - model predictions
+    ground_truth, np array of bool (num_samples) - true labels
+
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    '''
+    precision = 0
+    recall = 0
+    accuracy = 0
+    f1 = 0
+    
+    
+    fn = 0
+    tn = 0
+    tp = 0
+    fp = 0
+    
+    for i in range(len(prediction)):
+        if (prediction[i] and ground_truth[i]):
+            tp += 1
+        if (prediction[i] and not ground_truth[i]):
+            fp += 1
+        if (not prediction[i] and ground_truth[i]):
+            fn += 1
+        if (not prediction[i] and not ground_truth[i]):
+            tn += 1
+    
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    accuracy = (tp + tn)/(tp + tn + fp + fn)
+    f1 = 2 * precision*recall/(precision + recall)
+    
+    # TODO: implement metrics!
+    # Some helpful links:
+    # https://en.wikipedia.org/wiki/Precision_and_recall
+    # https://en.wikipedia.org/wiki/F1_score
+    
+    
+    return precision, recall, f1, accuracy
+
+
+def multiclass_accuracy(prediction, ground_truth):
+    '''
+    Computes metrics for multiclass classification
+
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    '''
+    # TODO: Implement computing accuracy
+    
+    truePred = 0
+    for i in range(len(prediction)):
+        if (prediction[i] == ground_truth[i]):
+            truePred += 1
+    
+    
+    
+    return truePred/len(prediction)
